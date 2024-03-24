6bab7207c11bdd56d4f619a46594d7120abb331f
Joselu <joseluissgz97@gmail.com>
2024-03-24 12:31:01 +0100

    add: adding taskToEdit to edit a task

--- src/components/TaskForms.tsx
+++ src/components/TaskForms.tsx
@@ -1,17 +1,45 @@
-import { useDispatch } from 'react-redux';
-import { addTask } from '../features/tasks/taskSlice';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../app/store';
+import { addTask, editTask } from '../features/tasks/taskSlice';
 import useTask from '../lib/hooks/useTask';
 import style from './TaskForm.module.scss';
 
 const TaskForms = () => {
-  const { title, description, setTitle, setDescription } = useTask();
+  const { title, description, setTitle, setDescription, setID } = useTask();
 
   const dispatch = useDispatch();
+  const taskList = useSelector((state: RootState) => state.tasks.tasks);
+
+  const taskToEdit = useSelector((state: RootState) => state.tasks.taskID);
+
+  useEffect(() => {
+    setTaskToEdit(taskToEdit);
+  }, [taskToEdit]);
+
+  const setTaskToEdit = (taskToEdit: string) => {
+    if (!taskToEdit) return;
+    const task = taskList.find((task) => task.id === taskToEdit);
+
+    if (!task) return;
+    setTitle(task.title);
+    setDescription(task.description);
+    setID(task.id);
+  };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
+    if (taskToEdit) {
+      dispatch(editTask({ id: taskToEdit, title, description }));
+      setDescription('');
+      setTitle('');
+      return;
+    }
+
     dispatch(addTask({ id: crypto.randomUUID(), title, description }));
+    setDescription('');
+    setTitle('');
   };
   return (
     <>
@@ -32,7 +60,7 @@ const TaskForms = () => {
           onChange={(e) => setDescription(e.target.value)}
         ></textarea>
 
-        <input type="submit" value="Add" />
+        <input type="submit" value={taskToEdit ? 'Edit' : 'Add'} />
       </form>
     </>
   );
