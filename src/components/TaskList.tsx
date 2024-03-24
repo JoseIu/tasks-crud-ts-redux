10ceacb0278f5ad5129f9ecd2707548b1d1a1418
Joselu <joseluissgz97@gmail.com>
2024-03-24 12:31:32 +0100

    Fizx: fixing tasklist.map

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,21 +1,24 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from '../app/store';
 
-import { deleteTask } from '../features/tasks/taskSlice';
+import { deleteTask, setTaskID } from '../features/tasks/taskSlice';
 import style from './TaskList.module.scss';
 
 const TaskList = () => {
   const taskList = useSelector((state: RootState) => state.tasks);
 
+  console.log(taskList);
+
   const dispatch = useDispatch();
 
   return (
     <div className={style.tasks}>
-      {taskList.map((task) => (
+      {taskList.tasks.map((task) => (
         <article key={task.id} className={style.task}>
           <h2> {task.title} </h2>
           <p> {task.description} </p>
           <button onClick={() => handleDelete(task.id, dispatch)}>X</button>
+          <button onClick={() => handleEdit(task.id, dispatch)}>Edit</button>
         </article>
       ))}
     </div>
@@ -24,6 +27,9 @@ const TaskList = () => {
 
 const handleDelete = (id: string, dispatch: AppDispatch) => {
   dispatch(deleteTask(id));
+};
+const handleEdit = (id: string, dispatch: AppDispatch) => {
+  dispatch(setTaskID(id));
 };
 
 export default TaskList;
