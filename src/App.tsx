c893ea87831edad34a4dc7141390ceb54d3d842f
Joselu <joseluissgz97@gmail.com>
2024-03-24 12:29:31 +0100

    refactor: removing uselector on APP component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
-import { useSelector } from 'react-redux';
-
 import './App.scss';
-import { RootState } from './app/store';
 import TaskForms from './components/TaskForms';
 import TaskList from './components/TaskList';
 
 const App = () => {
-  const tasksSstate = useSelector((state: RootState) => state.tasks);
-
-  console.log(tasksSstate);
   return (
     <>
       <h1>TASKS CRUD REACT + TS + REDUX</h1>
